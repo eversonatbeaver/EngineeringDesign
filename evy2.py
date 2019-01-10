699ef8a5ffadb0a5f5dc82a1f58f7f072f0d3847
evydibble <36007225+evydibble@users.noreply.github.com>
2019-01-10 09:23:25 -0500

    Create evy2.py

---
+++ evy2.py
@@ -0,0 +1,8 @@
+#has code choose random option/restaurant
+import random
+
+#list all of the options
+restaurants = ['Tango Mango', 'Juice Press', 'The Bagel Table', 'Shake Shack', 'Panera', 'Wegmans', 'Oath', 'Sweetgreen', 'Johnys','Tender Greens', '11/11']
+
+#has code print random choice of restaurant
+print(random.choice(restaurants))
