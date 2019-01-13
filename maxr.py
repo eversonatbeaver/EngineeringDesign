9244aea5466752c6e82b1f6cdfba4be3c7c4a818
abbyspencer <46002639+abbyspencer@users.noreply.github.com>
2019-01-13 10:22:53 -0500

    Update maxr.py

--- maxr.py
+++ maxr.py
@@ -1,4 +1,4 @@
-def make_dragon():
+def make_star():
 	print("")
 	print("       \****__              ____          ")                                  
 	print("         |    *****\_      --/ *\-__       ")                                 
@@ -9,11 +9,11 @@ def make_dragon():
 	print("        |    _/  __/ )\ \ _____         *\ ")                                 
 	print("        |\__/   /    ^ ^       \_________| ")
 
-number_of_dragons = input("How many dragons? ")
+number_of_stars = input("How many stars? ")
 
-if number_of_dragons > 0:
-	for x in range(0, number_of_dragons):
-		make_dragon()
+if number_of_stars > 0:
+	for x in range(0, number_of_stars):
+		make_star()
 	print("")
 else:
 	print("There must be a valid number")
