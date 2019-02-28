984e08f3c66547ed67aec2cd5d6e569ad05dcfac
cdeweck <46002613+cdeweck@users.noreply.github.com>
2019-02-28 09:43:10 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+harder=1
+better=2
+faster=3
+stronger=4
+Daft_Punk = str(raw_input("Pick_a_Number"))
+if Daft_Punk == '1':
+  print("Harder")
+if Daft_Punk == '2':
+  print("Harder_Better")
+if Daft_Punk == '3':
+  print("Harder_Better_Faster")
+if Daft_Punk == '4':
+  print("Harder_Better_Faster_Stronger")
